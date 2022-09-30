c43c442ad0421e0186d33a7823f57ff20cd4ab11
Simbel0 <simbel.user@gmail.com>
2022-09-30 02:58:08 +0200

    Fix glitch with the choice box

--- scripts/objects/Choicebox.lua
+++ scripts/objects/Choicebox.lua
@@ -1,7 +1,7 @@
-local Choicebox_o, super = Class(Choicebox, false)
+local Choicebox_o, super = Class("Choicebox", true)
 
 function Choicebox_o:init(x, y, width, height, battle_box)
-    super:init(self, x, y, width, height)
+    super:init(self, x, y, width, height, battle_box)
 
     if Game:getFlag("plot", 0)==3 then
         self.heart = Assets.getTexture("player/heart_menu_reverse")
