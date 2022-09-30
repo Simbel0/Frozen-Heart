cd5258e6d82d8184cb2bf44e585e7d162a87a589
Simbel0 <simbel.user@gmail.com>
2022-10-01 01:49:15 +0200

    Better deal

--- scripts/battle/waves/bonus/deal.lua
+++ scripts/battle/waves/bonus/deal.lua
@@ -4,7 +4,12 @@ function deal:onStart()
     local sneo = self.encounter.sneo
     self.org_x=sneo.x
     Game.battle.timer:tween(2, sneo, {x=SCREEN_WIDTH+80}, "out-cubic")
-    self:spawnBullet("neo/pipis", Game.battle.arena.right+120, Game.battle.arena.bottom-Game.battle.arena.top, 0, 0, false, 80, false)
+    local pipis = self:spawnBullet("neo/pipis", Game.battle.arena.right+160, (Game.battle.arena.bottom-Game.battle.arena.top)-30, 0, 0, false, 80, false)
+    pipis.sprite.alpha=0
+    pipis.sprite:fadeTo(1, 0.5)
+    local pipis = self:spawnBullet("neo/pipis", Game.battle.arena.right+100, (Game.battle.arena.bottom-Game.battle.arena.top)+80, 0, 0, false, 100, false)
+    pipis.sprite.alpha=0
+    pipis.sprite:fadeTo(1, 0.5)
 end
 
 function deal:update()
