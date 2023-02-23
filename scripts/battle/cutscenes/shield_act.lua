9b4487616c22a0bc237d89ed3353e1ee170f70bd
Simbel0 <simbel.user@gmail.com>
2023-02-23 02:51:55 +0100

    Finished the shield act

--- scripts/battle/cutscenes/shield_act.lua
+++ scripts/battle/cutscenes/shield_act.lua
@@ -18,10 +18,23 @@ return function(cutscene, user, target)
 	queen.glass = glass
 	queen:addChild(glass)
 
+	local acid1 = Sprite("wine_b")
+	acid1.cutout_top = 65 --48
+	glass:addChild(acid1)
+	local acid2 = Sprite("wine_t")
+	acid2.cutout_top = 50 --13
+	glass:addChild(acid2)
+
 	cutscene:wait(0.5)
 
+	Assets.playSound("queen_wine_appear")
 	Game.battle.timer:tween(0.5, glass, {x=148, y=137, scale_x=0.45/2, scale_y=0.45/2, color={1,0,0}})
+	local afimGlass = Game.battle.timer:everyInstant(1/16, function()
+        Game.battle:addChild(AfterImage(glass, 1))
+    end)
+
 	cutscene:wait(0.5)
+	Game.battle.timer:cancel(afimGlass)
 	glass:setLayer(BATTLE_LAYERS["above_bullets"])
 	Game.battle.timer:tween(0.55, Game.battle, {background_fade_alpha=0.75})
 
@@ -51,19 +64,33 @@ return function(cutscene, user, target)
 		end
 		glass.x=Utils.clamp(glass.x, 96, 200)
 
+		local function mapValue(input, inMin, inMax, outMin, outMax)
+	  		local inputRange = inMax - inMin
+	  		local outputRange = outMax - outMin
+	  		local scaledValue = (input - inMin) / inputRange
+	  		return outMin + scaledValue * outputRange
+		end
+
 		for i,v in ipairs(bullets) do
 			v.y = v.y+v.speed
+
+			if count == 6 and acid1.cutout_top>0 then
+				acid1.cutout_top = 0
+			end
+			if acid1.cutout_top == 0 then
+				acid2.cutout_top = mapValue(count, 7, 18, 50, 13)
+			end
 
 			if v:collidesWith(glass) then
 				v:remove()
 				table.remove(bullets, i)
+				Assets.stopAndPlaySound("swallow")
 				count = count+1
 			end
 
 			local x, y = v:getScreenPos()
 			if y>SCREEN_HEIGHT+20 then
 				v:remove()
-				Assets.stopAndPlaySound("swallow")
 				table.remove(bullets, i)
 			end
 		end
@@ -79,6 +106,8 @@ return function(cutscene, user, target)
 	bullets = nil
 
 	Game.battle.timer:tween(0.5, glass, {x=47, y=10, scale_x=0.2, scale_y=0.3, color={1,1,1}})
+	Game.battle.timer:tween(0.5, acid1, {color={0,1,0}})
+	Game.battle.timer:tween(0.5, acid2, {color={0,1,0}})
 	cutscene:wait(0.5)
 	glass:setLayer(queen.layer-1)
 	Game.battle.timer:tween(0.55, Game.battle, {background_fade_alpha=0})
@@ -87,9 +116,9 @@ return function(cutscene, user, target)
 	print("final count = "..level)
 	if level>=6 then
 		shield_size = 12
-	elseif level>=3 then
+	elseif level>=4 then
 		shield_size = 10
-	elseif level>=1 then
+	elseif level>=2 then
 		shield_size = 7
 	else
 		shield_size = 0
@@ -97,9 +126,25 @@ return function(cutscene, user, target)
 	print("shield level = "..shield_size)
 
 	if shield_size==0 then
-		--TODO
+		cutscene:wait(0.5)
+		Assets.playSound(Utils.pick({"queen/hoot", "queen/hoot_2", "queen/hoot_3"}))
+		local retro_explosion = RetroExplosion(70, 70)
+		Game.battle:addChild(retro_explosion)
+
+		local queen = Game.battle.encounter.queen
+		queen.glass:remove()
+		queen:setAnimation({"hurt", 1/14, true})
+		queen.physics.speed_x = -30
+		queen.physics.gravity = 1.5
+		queen.physics.gravity_direction = 0
+		queen.shield_broken = true
+		queen.shield = nil
+		cutscene:wait(function()
+			return not queen.shield_broken
+		end)
+		cutscene:endCutscene()
 	else
-		local shield = AcidShield(7, queen)
+		local shield = AcidShield(shield_size, queen)
 		queen:addChild(shield)
 		shield.appearcon = 1
 		queen.air_mouv = true
