19b67d3eede74332d935ce3a7ba6e0c7fd75762d
Simbel0 <simbel.user@gmail.com>
2023-09-20 23:13:47 +0200

    Pipis hits the arena correctly now

--- scripts/battle/bullets/secret/memories/pipis.lua
+++ scripts/battle/bullets/secret/memories/pipis.lua
@@ -16,7 +16,7 @@ end
 function pipis:update()
     super:update(self)
 
-    if self.y >= Game.battle.arena.top-self.sprite.height/2 then
+    if self.y >= Game.battle.arena:getTop()-self.sprite.height/2 then
         for i=1,5 do
             local bullet = self.wave:spawnBullet("neo/crewBullet", self.x, self.y, math.rad((math.deg(Utils.angle(self.x, self.y, Game.battle.soul.x, Game.battle.soul.y))+Utils.random(10, 20))-17), 10, nil, false, false, false)
             bullet:setScale(1)
