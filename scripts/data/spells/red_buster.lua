449f48119b2b6d9026fa42008bb95daf611bd34e
Simbel0 <simbel.user@gmail.com>
2024-06-18 05:26:05 +0200

    Made a funny thing for debug users

--- scripts/data/spells/red_buster.lua
+++ scripts/data/spells/red_buster.lua
@@ -1,5 +1,13 @@
 local spell, super = Class("red_buster", true)
 
+function spell:getCastMessage(user, target)
+    local message = super:getCastMessage(self, user, target)
+    if (Game.battle.encounter.id == "secret_battle" and Game.battle.encounter.intro) then
+        message = "[noskip]"..message.."\n[wait:5][speed:0.1]* ...[speed:1]But "..user.chara:getName().." couldn't see anything."
+    end
+    return message
+end
+
 function spell:onCast(user, target)
     local buster_finished = false
     local anim_finished = false
@@ -15,25 +23,31 @@ function spell:onCast(user, target)
     end
     Game.battle.timer:after(15/30, function()
         Assets.playSound("rudebuster_swing")
+        local full_miss = (Game.battle.encounter.id == "secret_battle" and Game.battle.encounter.intro)
         local x, y = user:getRelativePos(user.width, user.height/2 - 10, Game.battle)
         local tx, ty = target:getRelativePos(target.width/2, target.height/2, Game.battle)
+        if full_miss then
+            tx = 800
+        end
         local blast = RudeBusterBeam(true, x, y, tx, ty, function(pressed)
-            local damage = math.ceil((user.chara:getStat("magic") * 6) + (user.chara:getStat("attack") * 13) - (target.defense * 6)) + 90
-            if pressed then
-                damage = damage + 30
-                Assets.playSound("scytheburst")
-            end
-            if target.id == "ring_noelle" then
-                if not Game.battle.encounter.last_section then
-                    damage = love.math.random(2, 5)+(pressed and 2 or 0)
+            if not full_miss then
+                local damage = math.ceil((user.chara:getStat("magic") * 6) + (user.chara:getStat("attack") * 13) - (target.defense * 6)) + 90
+                if pressed then
+                    damage = damage + 30
+                    Assets.playSound("scytheburst")
                 end
-            elseif target.id == "Spamton_NEO" then
-                damage = Utils.round(damage/2)+(pressed and 30 or 0)
-            end
+                if target.id == "ring_noelle" then
+                    if not Game.battle.encounter.last_section then
+                        damage = love.math.random(2, 5)+(pressed and 2 or 0)
+                    end
+                elseif target.id == "Spamton_NEO" then
+                    damage = Utils.round(damage/2)+(pressed and 30 or 0)
+                end
 
-            local flash = target:flash()
-            flash.color_mask:setColor(1, 0, 0)
-            target:hurt(damage, user)
+                local flash = target:flash()
+                flash.color_mask:setColor(1, 0, 0)
+                target:hurt(damage, user)
+            end
             buster_finished = true
             if anim_finished then
                 Game.battle:finishAction()
--- scripts/data/spells/rude_buster.lua
+++ scripts/data/spells/rude_buster.lua
@@ -1,5 +1,13 @@
 local spell, super = Class("rude_buster", true)
 
+function spell:getCastMessage(user, target)
+    local message = super:getCastMessage(self, user, target)
+    if (Game.battle.encounter.id == "secret_battle" and Game.battle.encounter.intro) then
+        message = "[noskip]"..message.."\n[wait:5][speed:0.1]* ...[speed:1]But "..user.chara:getName().." couldn't see anything."
+    end
+    return message
+end
+
 function spell:onCast(user, target)
     local buster_finished = false
     local anim_finished = false
@@ -15,24 +23,30 @@ function spell:onCast(user, target)
     end
     Game.battle.timer:after(15/30, function()
         Assets.playSound("rudebuster_swing")
+        local full_miss = (Game.battle.encounter.id == "secret_battle" and Game.battle.encounter.intro)
         local x, y = user:getRelativePos(user.width, user.height/2 - 10, Game.battle)
         local tx, ty = target:getRelativePos(target.width/2, target.height/2, Game.battle)
+        if full_miss then
+            tx = 800
+        end
         local blast = RudeBusterBeam(false, x, y, tx, ty, function(pressed)
-            local damage = math.ceil((user.chara:getStat("magic") * 5) + (user.chara:getStat("attack") * 11) - (target.defense * 3))
-            if pressed then
-                damage = damage + 30
-                Assets.playSound("scytheburst")
-            end
-            if target.id == "ring_noelle" then
-                if not Game.battle.encounter.last_section then
-                    damage = love.math.random(2, 5)+(pressed and 1 or 0)
+            if not full_miss then
+                local damage = math.ceil((user.chara:getStat("magic") * 5) + (user.chara:getStat("attack") * 11) - (target.defense * 3))
+                if pressed then
+                    damage = damage + 30
+                    Assets.playSound("scytheburst")
                 end
-            elseif target.id == "Spamton_NEO" then
-                damage = Utils.round(damage/2)+(pressed and 30 or 0)
-            end
+                if target.id == "ring_noelle" then
+                    if not Game.battle.encounter.last_section then
+                        damage = love.math.random(2, 5)+(pressed and 1 or 0)
+                    end
+                elseif target.id == "Spamton_NEO" then
+                    damage = Utils.round(damage/2)+(pressed and 30 or 0)
+                end
 
-            target:flash()
-            target:hurt(damage, user)
+                target:flash()
+                target:hurt(damage, user)
+            end
             buster_finished = true
             if anim_finished then
                 Game.battle:finishAction()
