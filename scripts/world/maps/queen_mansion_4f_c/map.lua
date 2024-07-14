7e023ec9f6c62456c520c8d20fb0dbeb11dc54d7
Simbel0 <simbel.user@gmail.com>
2024-07-14 18:21:59 +0200

    Egg hint doesn't appear if conditions are not respected

--- scripts/world/maps/queen_mansion_4f_c/map.lua
+++ scripts/world/maps/queen_mansion_4f_c/map.lua
@@ -1,25 +1,29 @@
 local map, super = Class(Map)
 
 function map:onEnter()
-	self.egg_hint = Game.world.timer:everyInstant(60, function()
-		print("hint")
-		local sprite = Sprite("world/events/blocktree/block", 2520, 220)
-		Game.world:addChild(sprite)
-		sprite:setLayer(Game.world:parseLayer("Calque de Tuiles 1"))
-		sprite:setPhysics({
-			direction = math.rad(175),
-			speed = 3
-		})
-		sprite:setGraphics({
-			fade = 0.03,
-			fade_to = 0,
-			fade_callback = function() sprite:remove() end
-		})
-	end)
+	if not Game:getFlag("man_room", false) and Game:getFlag("noelle_battle_status", "") == "no_trance" then
+		self.egg_hint = Game.world.timer:everyInstant(60, function()
+			print("hint")
+			local sprite = Sprite("world/events/blocktree/block", 2520, 220)
+			Game.world:addChild(sprite)
+			sprite:setLayer(Game.world:parseLayer("Calque de Tuiles 1"))
+			sprite:setPhysics({
+				direction = math.rad(175),
+				speed = 3
+			})
+			sprite:setGraphics({
+				fade = 0.03,
+				fade_to = 0,
+				fade_callback = function() sprite:remove() end
+			})
+		end)
+	end
 end
 
 function map:onExit()
-	Game.world.timer:cancel(self.egg_hint)
+	if self.egg_hint then
+		Game.world.timer:cancel(self.egg_hint)
+	end
 end
 
 return map
